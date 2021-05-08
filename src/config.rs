4d5d882c55f734899ad0c174359de047a2c7bd43
Maté Antunovic <just.mate.antunovic@gmail.com>
2021-05-08 17:52:29 +1200

    Improve configuration

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,33 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Config {
-    #[serde(default = "default_base_uri")]
-    pub base_uri: String,
+    #[serde(default = "default_base_url")]
+    pub base_url: String,
+    #[serde(default = "default_group_id")]
     pub group_id: String,
+    #[serde(default = "default_api_key")]
     pub api_key: String
 }
 
-fn default_base_uri() -> String {
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            base_url: default_base_url(),
+            group_id: default_group_id(),
+            api_key: default_api_key()
+        }
+    }
+}
+
+fn default_base_url() -> String {
     "https://api.schnooty.com/".to_owned()
+}
+
+fn default_group_id() -> String {
+    "main".to_owned()
+}
+
+fn default_api_key() -> String {
+    String::new()
 }
